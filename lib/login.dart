b151f6038ba3c9b96f69d223945f9fb555779446
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2024-04-11 20:29:58 +0900

    login, register

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
 import 'package:flutter/material.dart';
 import 'signup.dart';
 import 'find_id.dart';
@@ -10,9 +14,77 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _idController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
   bool _isObscure = true;
+
+  void Login(BuildContext context) {
+    _loginUser().then((response) {
+      print(response.body);
+      if(response.statusCode == 200){
+        final Map<String, dynamic> responseData = jsonDecode(response.body);
+        final String name = responseData['name'];
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('로그인'),
+              content: Text(name + '님 환영합니다.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Home()),
+                    );
+                  },
+                  child: Text('확인'),
+                ),
+              ],
+            );
+          },
+        );
+      }else{
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text(response.body),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('OK'),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+    });
+  }
+
+  Future<http.Response> _loginUser() async {
+    final url = Uri.http('43.202.127.16:8080','/api/v1/members/login');
+
+    final response = await http.post(
+      url,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'email': _idController.text,
+        'password': _passwordController.text,
+      }),
+    );
+
+    return response;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,6 +130,7 @@ class _LoginPageState extends State<LoginPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         TextField(
+          controller: _idController,
           decoration: InputDecoration(
             hintText: "아이디",
             border: OutlineInputBorder(
@@ -98,10 +171,7 @@ class _LoginPageState extends State<LoginPage> {
         ElevatedButton(
           onPressed: () {
             // 로그인 버튼이 클릭되었을 때 Home.dart 페이지로 이동
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Home()), 
-            );
+            Login(context);
           },
           child: Text(
             "로그인",
