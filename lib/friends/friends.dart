7795733d97a22aac67a5b787327a5be9ef92903e
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2024-05-01 00:18:16 +0900

    fix

--- lib/friends/friends.dart
+++ lib/friends/friends.dart
@@ -37,8 +37,18 @@ class _FriendsState extends State<Friends> {
   static final String memberUrl = "43.202.127.16:8080";
   static final String friendUrl = "43.202.127.16:8081";
 
-  static Future<http.Response> _searchFriend(String email) async{
-    final url = Uri.http(memberUrl,'/api/v1/members/'+email);
+  static String friendEmail = "";
+  static String friendName = "";
+
+  void _updateFriend(String email, String name){
+    setState(() {
+      friendEmail = email;
+      friendName = name;
+    });
+  }
+
+  void _searchFriend(String email) async{
+    final url = Uri.http(memberUrl,'/api/v1/members/search/'+email);
 
     final response = await http.get(
       url,
@@ -47,8 +57,15 @@ class _FriendsState extends State<Friends> {
         'Content-Type': 'application/json',
       }
     );
-
-    return response;
+    if(response.statusCode == 200){
+      print(response.body);
+      final Map<String, dynamic> responseData = jsonDecode(response.body);
+      _updateFriend(responseData['email'], responseData['name']);
+    }else{
+      print(response.body);
+      _updateFriend("", "");
+    }
+    print(friendName + " " + friendEmail);
   }
 
   static Future<http.Response> _getFriendsList() async{
@@ -218,123 +235,143 @@ class _FriendsState extends State<Friends> {
       ),
     );
   }
-}
 
-/// 친구추가 하단바
-void _showSlidingPanel(BuildContext context) {
-  final TextEditingController _emailController = TextEditingController();
-  showModalBottomSheet(
-    context: context,
-    backgroundColor: Colors.transparent,
-    builder: (context) => SlidingUpPanel(
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(20.0),
-        topRight: Radius.circular(20.0),
-      ),
-      minHeight: 1500,
-      maxHeight: MediaQuery.of(context).size.height* 0.7,
-      panel: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '친구 추가',
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                IconButton(
-                  icon: Icon(Icons.close),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            ),
-            SizedBox(height: 5.0),
-            Text(
-              '이메일로 친구를 추가해보세요!',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: '이메일 작성',
-                      border: OutlineInputBorder(),
+  /// 친구추가 하단바
+  void _showSlidingPanel(BuildContext context) {
+    final TextEditingController _emailController = TextEditingController();
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (context) => SlidingUpPanel(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+        minHeight: 1500,
+        maxHeight: MediaQuery.of(context).size.height* 0.7,
+        panel: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '친구 추가',
+                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 5.0),
+              Text(
+                '이메일로 친구를 추가해보세요!',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: '이메일 작성',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(width: 10.0),
-                ElevatedButton(
-                  onPressed: () {
-                    // _FriendsState._searchFriend()
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
+                  SizedBox(width: 10.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      _searchFriend(_emailController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      backgroundColor: Colors.black,
                     ),
-                    backgroundColor: Colors.black,
+                    child: Text(
+                      '검색',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                  child: Text(
-                    '검색',
-                    style: TextStyle(color: Colors.white),
-                  ),
+                ],
+              ),
+              SizedBox(height: 30.0),
+              Text(friendName,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
-              ],
-            ),
-            SizedBox(height: 30.0),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
+
+  /// 맵리스트 하단바
+  void _showSlidingPanel2(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (context) => SlidingUpPanel(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+        minHeight: 1500,
+        maxHeight: MediaQuery.of(context).size.height * 0.7,
+        panel: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'xxx의 맵 리스트',
+                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 5.0),
+              Text(
+                'xxx님과 함께하는 맵 리스트에요!',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-/// 맵리스트 하단바
-void _showSlidingPanel2(BuildContext context) {
-  showModalBottomSheet(
-    context: context,
-    backgroundColor: Colors.transparent,
-    builder: (context) => SlidingUpPanel(
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(20.0),
-        topRight: Radius.circular(20.0),
-      ),
-      minHeight: 1500,
-      maxHeight: MediaQuery.of(context).size.height * 0.7,
-      panel: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'xxx의 맵 리스트',
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                ),
-                IconButton(
-                  icon: Icon(Icons.close),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            ),
-            SizedBox(height: 5.0),
-            Text(
-              'xxx님과 함께하는 맵 리스트에요!',
-              style: TextStyle(fontSize: 16.0),
-            ),
-          ],
-        ),
-      ),
+Widget _inputField(BuildContext context, TextEditingController controller, IconData icon, String hintText, {bool isPassword = false}) {
+  return TextField(
+    controller: controller,
+    obscureText: isPassword,
+    decoration: InputDecoration(
+      hintText: hintText,
+      prefixIcon: Icon(icon),
+      border: InputBorder.none,
     ),
   );
 }
